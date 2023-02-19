979a95e0bc05bb6b16b5a455c8dab672c16dc46e
Chanshirecat <happiness.delight@outlook.com>
2023-02-19 20:01:56 +0100

    added Card component

--- Korean_memory/pages/game.vue
+++ Korean_memory/pages/game.vue
@@ -2,50 +2,39 @@
     <div>
       <h1>Hangeul Memory</h1>
       <section class="game-board">
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-      </section>"
+        <Card
+        v-for="(card, index) in cardList" 
+        :key="`card${index}`"
+        :value="card"
+        />
+      </section>
+      
       <button>Start Game</button>
     </div>
   </template>
+
+  <script>
+import Card from '~~/components/Card.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  },
+  setup() {
+    const cardList = []
+
+    for (let i = 0; i < 40; i++) {
+      cardList.push(i)
+    }
+
+    return {
+      cardList
+    }
+  }
+}
+
+</script>
 
 <style>
 #app {
