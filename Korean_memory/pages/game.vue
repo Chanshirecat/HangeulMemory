f1d483e7264c51a92676949261adbea0e91b4288
Chanshirecat <happiness.delight@outlook.com>
2023-02-19 22:01:14 +0100

    generating deck

--- Korean_memory/pages/game.vue
+++ Korean_memory/pages/game.vue
@@ -62,14 +62,23 @@ export default {
         })
     }
 
-    for (let i = 0; i < 40; i++) {
-      cardList.value.push({
-        value: i,
+    const cardItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
+
+    cardItems.forEach(item => {
+        cardList.value.push({
+        value: item,
         visible: false,
-        position: i,
+        position: null,
         match: false,
       })
-    }
+
+        cardList.value.push({
+        value: item,
+        visible: false,
+        position: null,
+        match: false,
+      })
+    })
 
     const flipCard = (payload) => {
       cardList.value[payload.position].visible = true;
