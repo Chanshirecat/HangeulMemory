bd4a24c7bf417617887855db23b2465fcc6e301c
Chanshirecat <happiness.delight@outlook.com>
2023-02-19 18:00:24 +0100

    added game board with colours

--- Korean_memory/pages/game.vue
+++ Korean_memory/pages/game.vue
@@ -1,7 +1,68 @@
 <template>
     <div>
       <h1>Hangeul Memory</h1>
+      <section class="game-board">
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+      </section>"
       <button>Start Game</button>
     </div>
   </template>
+
+<style>
+#app {
+}
+
+.card {
+  border: 40px solid #0F64CD;
+  border-radius: 100px;
+}
+
+.game-board {
+  display: grid;
+  grid-template-columns: 50px 50px 50px 50px 50px 50px 50px 50px;
+  grid-template-rows: 50px 50px 50px 50px 50px;
+  grid-column-gap: 55px;
+  grid-row-gap: 55px;
+  justify-content: center;
+}
+</style>
   
