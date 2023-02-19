86b92289b20bbca4254a0c1c6ee079c5b714102f
Chanshirecat <happiness.delight@outlook.com>
2023-02-19 21:09:55 +0100

    track picked cards, validate if true or false

--- Korean_memory/pages/game.vue
+++ Korean_memory/pages/game.vue
@@ -5,19 +5,19 @@
         <Card
         v-for="(card, index) in cardList" 
         :key="`card${index}`"
+        :match="card.match"
         :value="card.value"
         :visible="card.visible"
         :position="card.position"
         @select-card="flipCard"
         />
       </section>
-
-      <button>Start Game</button>
+      <h2>{{ status }}</h2>
     </div>
   </template>
 
   <script>
-  import { ref } from 'vue'
+  import { ref, watch } from 'vue'
 import Card from '~~/components/Card.vue';
 
 export default {
@@ -27,26 +27,65 @@ export default {
   },
   setup() {
     const cardList = ref([])
+    const userSelection = ref([])
+    const status = ref('')
 
     for (let i = 0; i < 40; i++) {
       cardList.value.push({
         value: i,
         visible: false,
-        position: 
+        position: i,
+        match: false,
       })
     }
 
     const flipCard = (payload) => {
       cardList.value[payload.position].visible = true;
+    
+    if (userSelection.value[0]) {
+      userSelection.value[1] = payload
+    } else {
+      userSelection.value[0] = payload
     }
+  }
+
+  watch(userSelection, (currentValue) => {
+    if (currentValue.length === 2) {
+      const firstCard = currentValue[0]
+      const secondCard = currentValue[1]
+
+      if (firstCard.faceValue === secondCard.faceValue) {
+        status.value = "Match!"
+
+        cardList.value[firstCard.position].
+        match = true
+        cardList.value[secondCard.position].
+        match = true
+
+      } else {
+        status.value = "Mismatch!"
+
+      cardList.value[firstCard.position].visible = firstCard.visible
+      = false
+      cardList.value[secondCard.position].visible = secondCard.visible
+      = false
+      }
+
+      userSelection.value.length = 0
+    } 
+  }, 
+  {deep: true }
+  )
 
     return {
       cardList,
       flipCard,
+      userSelection,
+      status,
+
     }
   }
 }
-
 </script>
 
 <style>
